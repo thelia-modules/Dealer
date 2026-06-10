3c3eebd90473aeea0a106742c4b62488e67a8017
Alexandre Nozière <anoziere@openstudio.fr>
2026-06-10 15:27:30 +0200

    feat: convert content/folder/brand/product tab-content hooks to Twig
    Render the back-office entity-edit Dealer tabs as Twig fragments for the
    default-twig BO. Reproduce the linked-dealer loop and supply the link
    form views from the hook instead of Smarty {form}/{loop} helpers.

--- Hook/TheliaAdminHook.php
+++ Hook/TheliaAdminHook.php
@@ -13,8 +13,23 @@
 
 namespace Dealer\Hook;
 
+use Dealer\Form\BrandLinkForm;
+use Dealer\Form\ContentLinkForm;
+use Dealer\Form\FolderLinkForm;
+use Dealer\Form\ProductLinkForm;
+use Dealer\Model\DealerQuery;
+use Dealer\Model\Map\DealerBrandTableMap;
+use Dealer\Model\Map\DealerContentTableMap;
+use Dealer\Model\Map\DealerFolderTableMap;
+use Dealer\Model\Map\DealerProductTableMap;
+use Dealer\Model\Map\DealerTableMap;
+use Propel\Runtime\ActiveQuery\Criteria;
+use Propel\Runtime\ActiveQuery\Join;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Thelia\Core\Event\Hook\HookRenderEvent;
+use Thelia\Core\Form\TheliaFormFactory;
 use Thelia\Core\Hook\BaseHook;
+use Thelia\Core\Template\Parser\ParserResolver;
 
 /**
  * Class TheliaAdminHook
@@ -22,53 +37,132 @@ use Thelia\Core\Hook\BaseHook;
  */
 class TheliaAdminHook extends BaseHook
 {
+    public function __construct(
+        private readonly TheliaFormFactory $dealerFormFactory,
+        ?EventDispatcherInterface $dispatcher = null,
+        ?ParserResolver $parserResolver = null,
+    ) {
+        parent::__construct($dispatcher, $parserResolver);
+    }
+
     public static function getSubscribedHooks(): array
     {
         return [
             'content.tab-content' => [['type' => 'back', 'method' => 'onContentModuleTab']],
-            'content.edit-js' => [['type' => 'back', 'method' => 'onContentEditJs']],
             'folder.tab-content' => [['type' => 'back', 'method' => 'onFolderModuleTab']],
-            'folder.edit-js' => [['type' => 'back', 'method' => 'onFolderEditJs']],
             'brand.tab-content' => [['type' => 'back', 'method' => 'onBrandModuleTab']],
-            'brand.edit-js' => [['type' => 'back', 'method' => 'onBrandEditJs']],
             'product.tab-content' => [['type' => 'back', 'method' => 'onProductModuleTab']],
-            'product.edit-js' => [['type' => 'back', 'method' => 'onProductEditJs']],
         ];
     }
 
-    public function onContentModuleTab(HookRenderEvent $event)
+    public function onContentModuleTab(HookRenderEvent $event): void
     {
-        $event->add($this->render("hook/content.html",$event->getArguments()));
+        $contentId = $event->getArgument('content_id');
+
+        $event->add($this->render('Dealer/hook/content.html.twig', [
+            'dealer_content_id' => $contentId,
+            'dealer_current_url' => $this->getCurrentUrl(),
+            'dealer_all_dealers' => $this->getAllDealers(),
+            'dealer_linked_dealers' => $this->getLinkedDealers(DealerContentTableMap::COL_DEALER_ID, DealerContentTableMap::COL_CONTENT_ID, $contentId),
+            'dealer_content_link_form' => $this->dealerFormFactory->createForm(ContentLinkForm::getName())->createView()->getView(),
+        ]));
     }
 
-    public function onContentEditJs(HookRenderEvent $event){
-        $event->add($this->render("script/dealer-content-js.html",$event->getArguments()));
-    }
-
-    public function onFolderModuleTab(HookRenderEvent $event)
+    public function onFolderModuleTab(HookRenderEvent $event): void
     {
-        $event->add($this->render("hook/folder.html",$event->getArguments()));
+        $folderId = $event->getArgument('folder_id');
+
+        $event->add($this->render('Dealer/hook/folder.html.twig', [
+            'dealer_folder_id' => $folderId,
+            'dealer_current_url' => $this->getCurrentUrl(),
+            'dealer_all_dealers' => $this->getAllDealers(),
+            'dealer_linked_dealers' => $this->getLinkedDealers(DealerFolderTableMap::COL_DEALER_ID, DealerFolderTableMap::COL_FOLDER_ID, $folderId),
+            'dealer_folder_link_form' => $this->dealerFormFactory->createForm(FolderLinkForm::getName())->createView()->getView(),
+        ]));
     }
 
-    public function onFolderEditJs(HookRenderEvent $event){
-        $event->add($this->render("script/dealer-folder-js.html",$event->getArguments()));
-    }
-
-    public function onBrandModuleTab(HookRenderEvent $event)
+    public function onBrandModuleTab(HookRenderEvent $event): void
     {
-        $event->add($this->render("hook/brand.html",$event->getArguments()));
+        $brandId = $event->getArgument('brand_id');
+
+        $event->add($this->render('Dealer/hook/brand.html.twig', [
+            'dealer_brand_id' => $brandId,
+            'dealer_current_url' => $this->getCurrentUrl(),
+            'dealer_all_dealers' => $this->getAllDealers(),
+            'dealer_linked_dealers' => $this->getLinkedDealers(DealerBrandTableMap::COL_DEALER_ID, DealerBrandTableMap::COL_BRAND_ID, $brandId),
+            'dealer_brand_link_form' => $this->dealerFormFactory->createForm(BrandLinkForm::getName())->createView()->getView(),
+        ]));
     }
 
-    public function onBrandEditJs(HookRenderEvent $event){
-        $event->add($this->render("script/dealer-brand-js.html",$event->getArguments()));
-    }
-
-    public function onProductModuleTab(HookRenderEvent $event)
+    public function onProductModuleTab(HookRenderEvent $event): void
     {
-        $event->add($this->render("hook/product.html",$event->getArguments()));
+        $productId = $event->getArgument('product_id');
+
+        $event->add($this->render('Dealer/hook/product.html.twig', [
+            'dealer_product_id' => $productId,
+            'dealer_current_url' => $this->getCurrentUrl(),
+            'dealer_all_dealers' => $this->getAllDealers(),
+            'dealer_linked_dealers' => $this->getLinkedDealers(DealerProductTableMap::COL_DEALER_ID, DealerProductTableMap::COL_PRODUCT_ID, $productId),
+            'dealer_product_link_form' => $this->dealerFormFactory->createForm(ProductLinkForm::getName())->createView()->getView(),
+        ]));
     }
 
-    public function onProductEditJs(HookRenderEvent $event){
-        $event->add($this->render("script/dealer-product-js.html",$event->getArguments()));
+    private function getCurrentUrl(): string
+    {
+        return $this->getRequest()?->getRequestUri() ?? '';
+    }
+
+    /**
+     * @return array<int, array{id: int, title: string}>
+     */
+    private function getAllDealers(): array
+    {
+        $locale = $this->getCurrentLocale();
+
+        $dealers = [];
+        foreach (DealerQuery::create()->orderById()->find() as $dealer) {
+            $dealers[] = [
+                'id' => $dealer->getId(),
+                'title' => $dealer->setLocale($locale)->getTitle(),
+            ];
+        }
+
+        return $dealers;
+    }
+
+    /**
+     * Dealers linked to the given entity, mirroring the legacy {loop type="dealer" <entity>_id=...}.
+     *
+     * @return array<int, array{id: int, title: string}>
+     */
+    private function getLinkedDealers(string $dealerColumn, string $entityColumn, mixed $entityId): array
+    {
+        if ($entityId === null || $entityId === '') {
+            return [];
+        }
+
+        $locale = $this->getCurrentLocale();
+
+        $join = new Join(DealerTableMap::COL_ID, $dealerColumn, Criteria::LEFT_JOIN);
+
+        $query = DealerQuery::create()
+            ->addJoinObject($join)
+            ->where($entityColumn . ' = ?', (int) $entityId, \PDO::PARAM_INT)
+            ->orderById();
+
+        $dealers = [];
+        foreach ($query->find() as $dealer) {
+            $dealers[] = [
+                'id' => $dealer->getId(),
+                'title' => $dealer->setLocale($locale)->getTitle(),
+            ];
+        }
+
+        return $dealers;
+    }
+
+    private function getCurrentLocale(): string
+    {
+        return $this->getRequest()?->getSession()?->getLang()?->getLocale() ?? 'en_US';
     }
 }
